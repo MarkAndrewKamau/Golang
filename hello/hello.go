de2f2ef233748f377f7a3578936abf7c9c241273
MarkAndrewKamau <kamaumark19@gmail.com>
2025-10-02 08:54:36 +0300

    feat: handle errors returned by greetings.Hello using log package

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,26 @@ package main   // Makes file executable program
 
 import (
 	"fmt"
+	"log"
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.setFlags(0)
+
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.println(message)
 }
