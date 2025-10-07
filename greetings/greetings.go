ab201fcd45ddc05dc1e93eb37f56ff3d83570e9f
MarkAndrewKamau <kamaumark19@gmail.com>
2025-10-07 16:54:41 +0000

    Add slices to return random messages

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
     "errors"
     "fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
@@ -11,7 +12,22 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. welcome!", name)
+
+	// Create a message using a random format
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
 
+//randomFormat returns one of a set of greeting messages.
+// The returned message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string {
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format.
+	return formats[rand.Intn(len(formats))]
+}
